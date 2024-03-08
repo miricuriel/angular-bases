e8f8ef3e4cffaa44db51ae76b12824417cc1088f
Miriam Curiel <curiel.miriam@gmail.com>
2024-03-08 00:08:28 -0600

    Sservicio privado

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
 import { DbzService } from '../services/dbz.service';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,7 +8,15 @@ import { DbzService } from '../services/dbz.service';
 })
 
 export class DbzComponent {
-constructor(public dbzService: DbzService){
-
+constructor(private dbzService: DbzService){
+}
+get characters(): Character[]{
+  return [...this.dbzService.characters];
+}
+onDeleteCharacter(id:string):void{
+  this.dbzService.deleteCharacteById(id);
+}
+onNewCharacter(character: Character):void{
+  this.dbzService.addCharacter(character);
 }
 }
