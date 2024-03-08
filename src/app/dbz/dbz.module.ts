254ff30c142b0aa81c15b0fc59c967441645535e
Miriam Curiel <curiel.miriam@gmail.com>
2024-03-07 19:09:51 -0600

    Generación de dos componentes con extensión -AngularSchematics- y con CLI -ng g c nameComponent-

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DbzComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { CharacterComponent } from './components/character/character.component';
 
 
 
 @NgModule({
   declarations: [
     DbzComponent,
+    ListComponent,
+    CharacterComponent,
   ],
   exports:[
     DbzComponent,
